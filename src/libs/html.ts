4074d627a7f9cc2c804e55959c90337de6c34d4f
rusconn <rusconn3@gmail.com>
2021-09-16 14:58:48 +0900

    Apply syntax highlights to posts codes

---
+++ src/libs/html.ts
@@ -0,0 +1,14 @@
+import cheerio from "cheerio";
+import hljs from "highlight.js/lib/common";
+
+const highlightCodes = (html: string) => {
+  const $ = cheerio.load(html);
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
+  return $.html();
+};
+
+export default highlightCodes;
