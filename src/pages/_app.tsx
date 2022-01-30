bfeff5fb3a6b8412531ab36dc49d5aec7052b399
rusconn <rusconn3@gmail.com>
2022-01-30 20:47:00 +0000

    refactor: use emotion instead of css modules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,31 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Global, css } from "@emotion/react";
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const globalStyles = css`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+      Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: royalblue;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Global styles={globalStyles} />
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
