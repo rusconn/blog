7b7c769e2ea5d10d6fd99e6141c3806cd566a938
rusconn <rusconn3@gmail.com>
2023-10-16 15:21:01 +0900

    chore(deps): upgrade astro and other needed packages

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,13 +1,13 @@
-import datefns from "date-fns";
-import datefnstz from "date-fns-tz";
+import { format, parseISO } from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
 
 type Props = {
   dateString: string;
 };
 
 export function Date({ dateString }: Props) {
-  const date = datefns.parseISO(dateString);
-  const localDate = datefnstz.utcToZonedTime(date, "Asia/Tokyo");
+  const date = parseISO(dateString);
+  const localDate = utcToZonedTime(date, "Asia/Tokyo");
 
-  return <time dateTime={dateString}>{datefns.format(localDate, "yyyy-MM-dd")}</time>;
+  return <time dateTime={dateString}>{format(localDate, "yyyy-MM-dd")}</time>;
 }
